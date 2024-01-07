a0898bf296b350c6153bf12f918b53a847416337
Azha Balouch <155921710+azhabalouch@users.noreply.github.com>
2024-01-08 03:20:15 +0500

    Uploading Portfolio Website.

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    var navList = document.querySelector('nav ul');
+    navList.style.display = (navList.style.display === 'none' || navList.style.display === '') ? 'flex' : 'none';
+  }
+  
